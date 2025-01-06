32045259dee454d52be062df1fd99532157fffa2
Nikita <nrdabari@gmail.com>
2025-01-06 10:57:04 +0530

    web socket connection establish

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors()); // Enable CORS
+
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+       },
+});
 
 io.on('connection', (socket) => {
- console.log('A user connected');
-socket.on('chat message', (msg) => {
- io.emit('chat message', msg); 
- });
-socket.on('disconnect', () => {
-console.log('A user disconnected');
-});
+     socket.on('chatMessage', (msg) => {
+        console.log('Message received:', msg);
+        io.emit('chatMessage', msg); 
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
 server.listen(8080, () => {
- console.log('Server is running on http://localhost:8080');
+    console.log('Server is running on http://localhost:8080');
 });
-
-
-
-
